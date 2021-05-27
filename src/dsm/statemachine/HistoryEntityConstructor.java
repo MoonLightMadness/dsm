24e2634e2ca4427fa6340a7aaadd7d3c304fc2d3
ZhangHL-q <qq2377459837@163.com>
2021-05-27 22:17:10 +0800

    优化事件

---
+++ src/dsm/statemachine/HistoryEntityConstructor.java
@@ -0,0 +1,15 @@
+package dsm.statemachine;
+
+import dsm.statemachine.entity.HistoryEntity;
+import dsm.utils.SimpleUtils;
+
+public class HistoryEntityConstructor {
+
+    public static HistoryEntity getEntity(String time,String eventName, int res){
+        HistoryEntity entity = new HistoryEntity();
+        entity.setTimestamp(time);
+        entity.setEventName(eventName);
+        entity.setRes(res);
+        return entity;
+    }
+}
