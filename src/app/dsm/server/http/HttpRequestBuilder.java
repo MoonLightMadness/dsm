9427db17a3d7e8edae3c181a2ea4b36e3327cfae
zhl <2377459837@qq.com>
2021-08-17 13:46:07 +0800

    完善HttpRequestBuilder

--- src/app/dsm/server/http/HttpRequestBuilder.java
+++ src/app/dsm/server/http/HttpRequestBuilder.java
@@ -1,5 +1,7 @@
 package app.dsm.server.http;
 
+import java.lang.reflect.Modifier;
+
 /**
  * @ClassName : app.dsm.server.http.HttpRequestBuilder
  * @Description :
@@ -8,8 +10,54 @@ package app.dsm.server.http;
  */
 public class HttpRequestBuilder {
 
+    /**
+     * 协议
+     */
+    private String protocol = "HTTP/1.1";
+
+    /**
+     * 模式
+     */
+    private String mode = "POST";
+
+    /**
+     * 请求路径
+     */
+    private String requestPath;
+
+    /**
+     * 数据
+     */
+    private String data;
+
+
     public HttpRequestBuilder setMode(String mode){
-        return null;
+        this.mode = mode;
+        return this;
+    }
+
+    public HttpRequestBuilder setProtocol(String protocol){
+        this.protocol = protocol;
+        return this;
+    }
+
+    public HttpRequestBuilder setRequestPath(String path){
+        this.requestPath=path;
+        return this;
+    }
+
+    public HttpRequestBuilder setData(String data){
+        this.data = data;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(mode).append(" ").append(requestPath).append(" ").append(protocol).append("\n");
+        sb.append("\n");
+        sb.append(data);
+        return sb.toString();
     }
 
 }
