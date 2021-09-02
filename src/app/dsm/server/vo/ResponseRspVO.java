813da4bd7d849ffe877a53e0af2105238569a6f8
zhl <2377459837@qq.com>
2021-09-02 14:41:32 +0800

    Response优化

---
+++ src/app/dsm/server/vo/ResponseRspVO.java
@@ -0,0 +1,19 @@
+package app.dsm.server.vo;
+
+import lombok.Data;
+
+/**
+ * @ClassName : app.dsm.server.vo.ResponseRspVO
+ * @Description :
+ * @Date 2021-09-02 14:37:37
+ * @Author ZhangHL
+ */
+@Data
+public class ResponseRspVO {
+
+
+    private String response;
+
+    private String msg;
+
+}
