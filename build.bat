5b9d674df3f42f69ddf8976be670974fe55a7aa9
zhl <2377459837@qq.com>
2021-09-02 09:51:11 +0800

    初始化配置脚本

---
+++ build.bat
@@ -0,0 +1,20 @@
+
+@echo off
+set meta=./metaconfig.txt
+
+@REM 如果不存在元配置文件，则创建一个新的
+if not  exist %meta% (
+    type nul > %meta%
+)
+
+echo #local_path = ./config/sample/sample.txt > %meta%
+
+@REM 接下来创建config及基础配置文件
+if not exist ./config (
+    md config
+    cd ./config
+    md sample
+    cd ./sample
+    echo #sample = sample > sample.txt
+)
+
